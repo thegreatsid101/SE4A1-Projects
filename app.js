6c237395c6d63d711c6f11437301074c7c3fcfc9
Ali Farooq <alifarooq122@gmail.com>
2022-06-28 21:49:51 +0500

    added user operation menu for different operations

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const prompt = require("prompt-sync")({ sigint: true });
+const arithmetic = require("./arithmetic");
 
-console.log("Calculator written in Node.js\n\n\n");
+console.log("\nCalculator written in Node.js\n\n");
 
-const firstNum = prompt("Enter first number: ");
-const secondNum = prompt("Enter second number: ");
+let firstNum = prompt("Enter first number: ");
+let secondNum = prompt("Enter second number: ");
 
+firstNum = parseInt(firstNum);
+secondNum = parseInt(secondNum);
+
+console.log("\n1 - Addition")
+console.log("2 - Subtraction")
+console.log("3 - Multiplication")
+console.log("4 - Division")
+
+let operation = prompt("\nSelect operation: ");
+operation = parseInt(operation);
+
+let result;
+
+switch (operation) {
+    case 1:
+        result = arithmetic.add(firstNum, secondNum);
+        console.log("Addition result: " + result);
+        break;
+    case 2:
+        result = arithmetic.sub(firstNum, secondNum);
+        console.log("Subtraction result: " + result);
+    case 3:
+        result = arithmetic.multiply(firstNum, secondNum);
+        console.log("Multiplication result: " + result);
+    case 4:
+        result = arithmetic.divide(firstNum, secondNum);
+        console.log("Division result: " + result);
+    default:
+        console.log("Invalid operation");
+}
