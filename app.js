56abc6311a634e4efa9db3868c76e204c9d25959
Ali Farooq <alifarooq122@gmail.com>
2022-06-28 21:04:21 +0500

    added functionality to take user input

---
+++ app.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+console.log("Calculator written in Node.js\n\n\n");
+
+const firstNum = prompt("Enter first number: ");
+const secondNum = prompt("Enter second number: ");
+
